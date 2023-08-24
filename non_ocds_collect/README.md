098f68af58dfc441c7950fa0796dc216eae451d6
Yohanna Lisnichuk <yohanitalisnichuk@gmail.com>
2023-08-24 09:52:48 -0400

    remove collect-generic

--- non_ocds_collect/README.md
+++
@@ -1,1 +0,0 @@
-## Non-OCDS data scraper
--- non_ocds_collect/non_ocds_scrapy/exporters.py
+++
@@ -1,9 +0,0 @@
-# https://stackoverflow.com/a/34487833
-from scrapy.exporters import CsvItemExporter
-
-
-class HeadlessCsvItemExporter(CsvItemExporter):
-    def __init__(self, file, *args, **kwargs):
-        # Include the header line if the current stream position is the start of the file.
-        kwargs["include_headers_line"] = file.tell() == 0
-        super().__init__(file, *args, **kwargs)
